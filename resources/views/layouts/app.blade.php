ffa90f9b41f4fa07494b69e899f3433f0897de9c
ZakaryAffani <48211021+ZakZomby@users.noreply.github.com>
2020-05-15 03:07:21 +0000

    implementing CRUD operations for categories with the use of request objects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,8 +73,16 @@
         </nav>
 
         <main class="py-4">
+
             @auth
                 <div class="container">
+
+                @if(session()->has('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                @endif
+
                     <div class="row">
                         <div class="col-md-4">
                             <ul class="list-group">
@@ -82,7 +90,7 @@
                                     <a href="">Post</a>
                                 </li>
                                 <li class="list-group-item">
-                                    <a href="">Categories</a>
+                                    <a href="{{ route('categories.index') }}">Categories</a>
                                 </li>
                             </ul>
                         </div>
@@ -96,5 +104,6 @@
             @endauth
         </main>
     </div>
+    @yield('scripts')
 </body>
 </html>
